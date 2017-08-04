bb5763fe5b422c1ac2b1afd9979a27008755aea9
Marwa Mahmoud <Marwa@dhcp-v100-086.mobile.uci.edu>
2017-08-03 18:57:43 -0700

    first user identified and he chooses the theme. reset button added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,22 +9,30 @@
   };
 
   firebase.initializeApp(config);
-  var database = firebase.database();
-  var storyRef = database.ref("/storyLines");
+
+  var database = firebase.database(); ////reference to database root
+  var storyRef = database.ref("/storyLines"); 
   var usersRef = database.ref("/Users");
   var startingSentence = database.ref("/Starting Sentences");
 
+  $("#resetBtn").show();
+
 
 ////////The login html page code///////////////// 
+
+///when the user logs in the user's name is pushed into the users database
 $("#login").on("click", function(){
 
   var userName = $("#nameInput").val().trim();
-  database.ref("/Users").push({userName: userName});
-
-  
+  database.ref("/Users").push({userName: userName}); 
 
 }); 
 
+
+///When new user is added to the database if this is the first user then he is moved
+///to the story themes page to choose a theme that applies to the rest of the players
+///if this is not the first player then he automatically moves to the story page with the
+///chosen theme
 usersRef.on("value", function(snap){
   var playersNum = snap.numChildren();
 
@@ -46,13 +54,18 @@ usersRef.on("value", function(snap){
 });
 
 /////////////////////////////////////////////////
+
+
 ////The stories themes html page code////////////
+
+//initializing an array with the starting sentences, a sentence for each theme
 var startingSentences = ["The night sky was full of stars but no moon.",
    "Lately, Marlene felt that there was something missing from her life.",
    "Lady Luck was always on my side at the casinos.",
    "The subway stopped suddenly inside the tunnel."];
 
- 
+///When the first player clicks on the story theme, the starting sentence related
+//that theme is picked from the database and displayed on the story page 
 $(".story-image").on("click", function(){
   var imageId = parseInt($(this).attr("id"));
   console.log(imageId);
@@ -62,25 +75,23 @@ $(".story-image").on("click", function(){
 
 });
 
-/////////////////////////////
+/////////////////////////////////////////////////////
 
-  //$("#storyDiv").html(startingSentences[imageId]);
-  //var sentence = "Once upon a time there was an avacado";
-  //storyRef.push(sentence);
+/////////The story page html code ////////////////
 
-  startingSentence.on("child_added", function(snap){
+
+//////When 
+startingSentence.on("child_added", function(snap){
 
     console.log(snap.val());
 
-      $("#storyDiv").html(snap.val());
+    $("#storyDiv").html(snap.val());
+
+});
 
 
-
-  });
-
-
-
-  $("#submitSentence").on("click", function(event){
+///When the user clicks write the sentence he entered is pushed to the database
+$("#submitSentence").on("click", function(event){
 
     event.preventDefault();
 
@@ -91,14 +102,19 @@ $(".story-image").on("click", function(){
     storyRef.push(newSentence);
     
 
-  });
+});
 
-  storyRef.on("child_added", function(snapshot){
+////When a new story sentence is added to the database it is automatically displayed 
+///in the story page
+storyRef.on("child_added", function(snapshot){
     $("#storyDiv").append("<br>"+snapshot.val());
 
-  });
+});
 
-  storyRef.on("value", function(snap){
+/////when the number of sentences reaches the limit, the users are notified that the
+////the coming sentence is the last one. When the last sentence is entered the players
+///cannot enter any more sentences
+storyRef.on("value", function(snap){
 
     console.log(snap.numChildren());
 
@@ -108,25 +124,26 @@ $(".story-image").on("click", function(){
     if(numSentences === 5){
       $("#storyDiv").append("This is the last sentence! Enter the ending, Make it count!");
 
-      //limitSentence = numSentences;
     }
 
     if(numSentences === (limitSentence+1)){
 
      $("#submitSentence").attr("disabled", true);
+
+     $("#resetBtn").show();
     }
-    //else{
-
-      //$("#submitSentence").attr("disabled", false);
-    //}
-
+   
 
   });
+
+////When the reset button is pressed the database is cleared  
+$("#resetBtn").on("click", function(){
+
+    database.ref().remove();
+ })
     
 
-
-
-
+///////////////////////////////////////////////////
 
 
 
